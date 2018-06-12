32fa9ffb973d82e90039fb1720f536b6a739545b
zavalzam <zavalzam@oregonstate.edu>
2018-06-12 11:46:48 -0700

    modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+var path = require('path');
 var express = require('express');
+var exphbs = require('express-handlebars');
 var app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'));
 
 app.listen(3000, function (err) {
   if (!err) {
