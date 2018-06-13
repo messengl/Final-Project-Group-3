8cb95126ffe709925a7993c7cb86adeaf8c1d631
zavalzam <zavalzam@oregonstate.edu>
2018-06-13 15:07:30 -0700

    changed some stuff

--- public/index.js
+++ public/index.js
@@ -1,1 +1,10 @@
 
+var modal = document.getElementById('modal');
+var backdrop = document.getElementById('modal-backdrop');
+
+function showmodal()  {
+  modal.classList.remove('hidden');
+  backdrop.classList.remove('hidden');
+}
+
+window.addEventListener('click', showmodal);
