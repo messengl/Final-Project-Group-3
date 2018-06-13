c73b15fc447c8e5ec057bc0df003167c8d8cf2ec
zavalzam <zavalzam@oregonstate.edu>
2018-06-13 15:59:19 -0700

    made stuff better

--- public/index.js
+++ public/index.js
@@ -1,10 +1,27 @@
-
+var bar = document.getElementById('bar');
+var pie = document.getElementById('pie');
+var scatter = document.getElementById('scatter');
+var table = document.getElementById('table');
 var modal = document.getElementById('modal');
 var backdrop = document.getElementById('modal-backdrop');
 
 function showmodal()  {
-  modal.classList.remove('hidden');
-  backdrop.classList.remove('hidden');
+  if(event.target.classList.contains('data-link')) {
+    modal.classList.remove('hidden');
+    backdrop.classList.remove('hidden');
+    bar.innerHTML = '<a href="bar/' + event.target.id + '">Bar Graph</a> ';
+    pie.innerHTML = '<a href="pie/' + event.target.id + '">Pie Chart</a> ';
+    scatter.innerHTML = '<a href="scatter/' + event.target.id + '">Scatter Plot</a> ';
+    table.innerHTML = '<a href="table/' + event.target.id + '">Table</a> ';
+  }
+  if(event.target.id == "close-modal-button") {
+    modal.classList.add('hidden');
+    backdrop.classList.add('hidden');
+    bar.innerHTML = "";
+    pie.innerHTML = "";
+    scatter.innerHTML = "";
+    table.innerHTML = "";
+  }
 }
 
-window.addEventListener('click', showmodal);
+window.addEventListener('click', showmodal)
