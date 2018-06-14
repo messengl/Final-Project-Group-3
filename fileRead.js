bdfd7c8ab38c79353d697e3f9479fd3d41a0e765
Garishtech <crisdmann@gmail.com>
2018-06-13 17:45:02 -0700

    Again

--- fileRead.js
+++ fileRead.js
@@ -1,12 +1,10 @@
-var parse = require("csv-parse/lib/sync");
-var fs = require('fs');
+module.exports = {
 
-console.log("===Starting up...");
-
-var data = fs.readFileSync('./data/Lottery_Powerball_Winning_Numbers__Beginning_2010.csv', 'utf-8');
-
-console.log("===Parsing...");
-
-var records = parse(data, {cast: true, column: true, trim: true});
-console.log(records[0], records [1]);
-
+function mycsvRead(file){
+	console.log("===Starting up...");
+	var data = fs.readFileSync(file, 'utf-8');
+	console.log("===Parsing...");
+	var records = parse(data, {cast: true, column: true, trim: true});
+	return data;
+}
+	}
