44a06547bff40b2af5f25e4aa17553f48aad85e3
Garishtech <crisdmann@gmail.com>
2018-06-13 17:43:45 -0700

    Finshed template, 1st attempt

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
+//var csvReading = require('./fileRead.js');
+var parse = require("csv-parse/lib/sync");
+var fs = require('fs');
+
+function csvRead(file){
+	console.log("===Starting up...");
+	var data = fs.readFileSync(file, 'utf-8');
+	console.log("===Parsing...");
+	var records = parse(data, {cast: true, column: true, trim: true});
+	return data;
+}
+
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars')
@@ -21,10 +34,27 @@ app.get('/scatter/:data', function (req, res, next) {
 });
 
 app.get('/table/:data', function (req, res, next) {
+	var tableLink;
 	
+	if(req.params.data == 'titanic')
+		tableLink = 'titanic';
+	if(req.params.data == 'death')
+		tableLink = 'deathRate';
+	if(req.params.data == 'birth')
+		tableLink = 'birthRate';
+	if(req.params.data == 'scorecards')
+		tableLink = 'scorecards';
+	if(req.params.data == 'lottery')
+		tableLink = 'lottery';
+
+	var tableData = csvRead('./data/' + tableLink + '.csv');	
 	
-	
-  res.send();
+	res.status(200).render('table',{
+			categories: tableData[0],
+			data: tableData
+		});
+		
+  //res.send();
 });
 
 app.listen(3000, function (err) {
